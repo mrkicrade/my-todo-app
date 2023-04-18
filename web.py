893f0f6f579eb6fcea2c9ce537948239a0c1a404
mrkicrade <mrkic78@gmail.com>
2023-04-18 17:10:20 +0200

    Initial commit

---
+++ web.py
@@ -0,0 +1,32 @@
+import streamlit as st # instaliran streamlit
+import functions
+
+# isntaliraj streamlit
+# aplikaciju pokreni iz terminala komandom streamlit run web.py
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+todos = functions.get_todos()
+
+st.title('My Todo App')
+st.subheader('This is my todo app')
+st.write('This add is to increase your productivity')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        # print(checkbox)
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+        # st.experimental_rerun() # probaj jednu od ove dve funkcije
+
+st.text_input(label='', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
+
+st.session_state
